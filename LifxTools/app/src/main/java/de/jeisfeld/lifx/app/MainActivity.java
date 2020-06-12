52a547adbe7f7e1ebf65d2c3db2b0e11dc37ec75
Jörg Eisfeld <github@jeisfeld.de>
2020-06-12 14:47:01 +0200

    Enable refresh button

--- LifxTools/app/src/main/java/de/jeisfeld/lifx/app/MainActivity.java
+++ LifxTools/app/src/main/java/de/jeisfeld/lifx/app/MainActivity.java
@@ -3,8 +3,10 @@ package de.jeisfeld.lifx.app;
 import com.google.android.material.navigation.NavigationView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -46,6 +48,19 @@ public class MainActivity extends AppCompatActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public final boolean onOptionsItemSelected(final MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.action_refresh:
+			Intent intent = getIntent();
+			finish();
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 	@Override
